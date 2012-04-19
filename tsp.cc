4e68a5e283cb64cc8f0696c958058fcd1bd9e2f6
Kelvin Lam <kelvin.j.lam@gmail.com>
2012-04-19 14:56:02 -0700

    implemented generate_tour, and added partition and quick_select methods for vector input

--- tsp.cc
+++ tsp.cc
@@ -11,15 +11,14 @@
 
 int num_cities;
 int populatoin_size;
-float greedy_selection_percentage;
+int greedy_selection_percentage;
 int num_closer_way_points;
 int group_size;
 float mutation_percent;
 int termination_step;
 int** distance_matrix;
 int ** closest_neighbors;
-tour* population;
-	//Tour new_population[]
+tour* populationu	//Tour new_population[]
 
 /*
  * Tour run():  generates initial population, then runs through
@@ -29,7 +28,7 @@ tour* population;
 public void run(){
 	num_cities = 10;
 	population_size = 10;
-	greedy_selection_percentage = 0.8;
+	greedy_selection_percentage = 80;
 	num_closer_way_points = 3;
 	group_size = 3;
 	mutation_percent = 0.7;
@@ -39,7 +38,6 @@ public void run(){
 	closest_neightbors = new int[num_cities][num_closer_way_points];
 	generate_distance_matrix();
 	generate_initial_population();
-
 }
 
 /*
@@ -91,11 +89,60 @@ int quick_select(int* input, int p, int r, int k)
 }
 
 /*
+*vector input version of partition
+*/
+int partition(vector<input> input, int p, int r){
+    int pivot = input[r];
+
+    while ( p < r )
+    {
+        while ( input[p] < pivot )
+            p++;
+
+        while ( input[r] > pivot )
+            r--;
+
+        if ( input[p] == input[r] )
+            p++;
+        else if ( p < r ) {
+            int tmp = input[p];
+            input[p] = input[r];
+            input[r] = tmp;
+        }
+    }
+
+    return r;
+}
+
+
+/*
+*vector version of quickselect
+*/
+int quick_select(vector<int> input, int p, int r, int k)
+{
+	while(){
+		if ( p == r )
+			return input[p];
+		int j = partition(input, p, r);
+		int length = j - p + 1;
+		if ( length == k )
+			return input[j];
+		else if ( k < length )
+			r = j-1;
+		else{
+			k -= length;
+			p = j + 1;
+		}
+	}
+}
+
+
+/*
  * generates a distance matrix in distance_matrix
  */
 public generate_distance_matrix(){
 	for(int i = 0; i < population_size; i++){
-
+		
 	}
 
 }
@@ -104,8 +151,13 @@ public generate_distance_matrix(){
  * 	calling generate tour many times,
  */
 public void generate_initial_population(){
-	for(int i =0; i < population_size; i++){
-		population[i] = generate_tour();
+	int *linear_cities = new int[num_cities];
+	for(int i = 0; i < num_cities; i++){
+		linear_cities[i] = i;
+	}
+	for(int j =0; j < population_size; j++){
+		population[j] = generate_tour(linear_cities);
+		
 	}
 }
 
@@ -116,11 +168,12 @@ public void generate_initial_population(){
 private void generate_closest_neighbors(){
 	for(int i = 0; i < population_size; i++){
 		int closest_index = 0;
-	    int m = quick_select(distance_matrix[i], 0, population_size - 1, num_closer_way_points);
+		//num_closer_way_points + 1 used as quick_select will pick out i as well, since distance is 0 with itself
+	    int m = quick_select(distance_matrix[i], 0, population_size - 1, num_closer_way_points + 1);
 	    closest_neighbors[i][closest_index] = m;
 	    closest_index++;
 		for(int k = 0; k < population_size; k++){
-			if(distance_matrix[i][k] < m){
+			if(distance_matrix[i][k] < m && distance_matrix[i][k] != 0 ){
 				closest_neighbors]i][closest_index] = distance_matrix[i][k];
 				closest_index++;
 				if(closest_index == num_closer_way_points){
@@ -138,8 +191,43 @@ private void generate_closest_neighbors(){
 	paper had 2 methods for picking the next waypoint in a tour that
 	were influenced by the greedy_selection_percentage
  */
-private tour generate_tour(){
-
+private tour generate_tour(int* linear_cities){
+	tour new_tour;
+	new_tour.fitness = 0;
+	new_tour.tour = new int[num_cities];
+	new_tour.tour_lengths = new int[num_cities];
+	vector<int> available_cities(linear_cities, linear_cities + sizeof(linear_cities)/sizeof(int));
+	new_tour.tour[0] = 0;
+	int current_city = 0;
+	for(int i = 1; i < num_cities; i ++){
+		if(i == 0){
+			available_cities.erase(
+		}
+		int selection = rand() % num_cities;
+		//choose greedily 
+		if(selection < greedy_selection_percentage && available_cities.size > num_closer_way_points){
+			//TODO: check that the ranodm selection producing desired values
+			int random_closest= rand() % (num_closer_way_points); 
+			int next_city = quickselect(available_cities, 0, availabe_cities.size() - 1, random_closest);
+			
+		}else // choose next city randomly 
+		{
+			int next_city = rand() % available_cities.size();
+		}
+		for(int j = 0; j < available_cities.size; j++){
+			if(available_cities[j] == next_city){
+				available_cities.erase(available_cities.begin() + j);
+				break;
+			}
+		}
+		new_tour.tour[i] = next_city;
+		new_tour.tour_lengths[i-1] = distance_matrix[current_city][next_city];
+		new_tour.fitness += distance_matrix[current_city][next_city];
+		current_city = next_city;
+	}
+	new_tour.tour_length[i] = distance_matrix[current_city][0];
+	new_tour.fitness += distance_matrix[current_city][next_city];
+	return tour;
 }
 
 /*
@@ -160,11 +248,12 @@ private int random_selection(int city){
 }
 
 /*
- * 	Tour[] select_group(int group_size): returns an array of length
+ * DEPRECATED, just uses quicksort 
+ * 	select_group(int group_size): quicksorts the population 
  * 	 group_size of the most optimal tours (run sort_population first
  * 	 to figure out best tours)
  */
-tour* select_group(int group_size){
+private void select_group(int group_size){
 
 }
 
